3068fe456c9ec8c33161fb0cc936cfd8529852d6
sabrinagiometto <sabrina.giometto@gmail.com>
2026-08-04 12:23:11 +0200

    parameters postprocessing updated

--- p_parameters/7_parameters_postprocessing.R
+++ p_parameters/7_parameters_postprocessing.R
@@ -1,21 +1,20 @@
 # name period
 
 name_period <- list()
-name_period[["1a"]] <- "No antidoto, prima delle restrizioni Covid"
-name_period[["1b"]] <- "No antidoto, durante le restrizioni Covid"
-name_period[["1c"]] <- "No antidoto, dopo le restrizioni Covid"
-name_period[["2"]] <- "Antidoto disponibile, prima delle linee guida"
-name_period[["3"]] <- "Antidoto disponibile, dopo le linee guida"
-name_period[["AllPeriod"]] <- "Totale"
+name_period[["pre"]] <- "Pre-Nota 100 AIFA: 1 gennaio 2016 - 25 gennaio 2022"
+name_period[["nota"]] <- "Nota 100 AIFA: 26 gennaio 2022 - 31 luglio 2025"
+name_period[["modi"]] <- "Modifica Nota 100 AIFA: 1 settembre 2025 - 31 dicembre 2025"
 
-name_outcome <- list()
-name_outcome[["outcome_DEATH"]] <- "Decesso"
-name_outcome[["outcome_IS"]] <- "Ictus ischemico"
-name_outcome[["outcome_VTE"]] <- "Trombosi venosa profonda"
-name_outcome[["outcome_TIA"]] <- "Attacco ischemico transitorio"
-name_outcome[["outcome_AMI"]] <- "Infarto del miocardio"
-name_outcome[["outcome_PE"]] <- "Embolia polmonare"
-name_outcome[["outcome_THROM"]] <- "Trombosi"
-name_outcome[["outcome_DIC"]] <- "Coagulazione intravascolare disseminata"
-name_outcome[["outcome_comp"]] <- "Trombosi o decesso"
+# name_period[["AllPeriod"]] <- "Totale"
+
+# name_outcome <- list()
+# name_outcome[["outcome_DEATH"]] <- "Decesso"
+# name_outcome[["outcome_IS"]] <- "Ictus ischemico"
+# name_outcome[["outcome_VTE"]] <- "Trombosi venosa profonda"
+# name_outcome[["outcome_TIA"]] <- "Attacco ischemico transitorio"
+# name_outcome[["outcome_AMI"]] <- "Infarto del miocardio"
+# name_outcome[["outcome_PE"]] <- "Embolia polmonare"
+# name_outcome[["outcome_THROM"]] <- "Trombosi"
+# name_outcome[["outcome_DIC"]] <- "Coagulazione intravascolare disseminata"
+# name_outcome[["outcome_comp"]] <- "Trombosi o decesso"
 

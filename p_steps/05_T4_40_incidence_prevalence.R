f9e1d2dc9f34d62e9dd5630b021da812a8e58e61
sabrinagiometto <sabrina.giometto@gmail.com>
2026-09-04 11:05:34 +0200

    denominator (total population) loaded and merged into the table including the numerator

--- p_steps/05_T4_40_incidence_prevalence.R
+++ p_steps/05_T4_40_incidence_prevalence.R
@@ -28,6 +28,8 @@ for (k in drug_names_s) {
   
 }
 
+D4_pop_ASL <- readRDS(file.path(thisdirinput, "D4_pop_ASL.rds"))
+
 # bind rows
 D4_prevalence_incidence <- rbindlist(mget(ls(pattern = "^D4_prevalence_incidence_")))
 
@@ -37,6 +39,11 @@ for (i in drug_names_s) {
 
 tab <- D4_prevalence_incidence[drug==i, .(prevalent = sum(is_prevalent),
                                incident = sum(is_incident)), .(year, ASL)]
+
+tab <- merge(tab, D4_pop_ASL, by = c("year", "ASL"), all = TRUE)
+
+tab[, `:=`(prevalence=prevalent/pop18,
+           incidence=incident/pop18)]
 
 assign(paste0("D5_prevalence_incidence_", i), tab)
 

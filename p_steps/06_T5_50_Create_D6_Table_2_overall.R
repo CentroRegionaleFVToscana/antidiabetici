bea00271abddae2f9736c5e0989185f26497d248
sabrinagiometto <sabrina.giometto@gmail.com>
2026-09-08 15:44:46 +0200

    D6 for Table 2 including only overall frequencies added

---
+++ p_steps/06_T5_50_Create_D6_Table_2_overall.R
@@ -0,0 +1,250 @@
+
+#### D6_Table_1_cohort_characteristics ----
+
+
+# authors: Rosa Gini, Sabrina Giometto
+
+# v 1.3
+
+# fixed input from direxp
+
+# v 1.2
+
+# adapting to study on antidiabetics
+
+# v 1.1
+
+# added name of data sources
+
+# v 1.0 
+
+# 11 Jun 2026
+
+# v 0.1
+
+# 8 Jun 2026
+
+
+print('CREATE D6_Table_2_cohort_characteristics_subpopulation_CAP')
+
+
+# assign directories
+
+if (TEST){ 
+  testname <- "test_D6_Table_2_overall"
+  thisdirinput <- paste0(file.path(dirtest, testname), "/")
+  thisdiroutput <- file.path(dirtest,testname,"g_output")
+  dir.create(thisdiroutput, showWarnings = F)
+}else{
+  thisdirinput <- direxp
+  thisdiroutput <- direxp
+}
+
+# load
+
+for (j in drug_names) {
+  
+  D5 <- read.csv(paste0(thisdirinput, "D5_Table_2_caratterizzazione_coorte_sottopopolazione_CAP_", j, "_overall.csv"))
+  
+  D5 <- as.data.table(D5)
+  
+  assign(paste0("D5_Table_2_caratterizzazione_coorte_sottopopolazione_CAP_",j,"_overall"), D5)
+
+}
+
+    
+# helpers
+
+add_empty_row <- function(j){
+  
+  j <- j + 1
+  tab_nice[, cell := ""]
+  setnames(tab_nice, "cell", paste0("cell_",j))
+  return(j)
+}
+
+descriptive_N_perc <- function(j, covar) {
+  
+  j <- j + 1
+  varN <- paste0(covar,"N")
+  varP <- paste0(covar,"p")
+  tab_nice[, cell := paste0(
+    formatC(get(varN), format = "f", digits = 0, big.mark = ".", 
+            decimal.mark = ","), " (",
+    formatC(get(varP), format = "f", digits = 1, big.mark = ".", 
+            decimal.mark = ","),"%)"
+  )]
+  setnames(tab_nice, "cell", paste0("cell_",j))
+  return(j)
+}
+
+descriptive_median_q1q3 <- function(j, covar) {
+  
+  j <- j + 1
+  varM <- paste0(covar,"_median")
+  varQ1 <- paste0(covar,"_q1")
+  varQ3 <- paste0(covar,"_q3")
+  tab_nice[, cell := paste0(get(varM), " (", get(varQ1), " - ", 
+                            get(varQ3), ")")]
+  setnames(tab_nice, "cell", paste0("cell_", j))
+  return(j)
+}
+
+
+
+#########################################
+# POPULATE ROWS
+
+for (k in drug_names) {
+
+  tab_nice <- copy(get(paste0("D5_Table_2_caratterizzazione_coorte_sottopopolazione_CAP_", k, "_overall")))
+   
+  # row 0
+  row_header_1 <- c()
+  j            <- -1
+  
+  # row 1
+  row_header_1 <- c(row_header_1, "Periodo")
+  j <- j + 1
+  tab_nice[, cell := period]
+  setnames(tab_nice, "cell", paste0("cell_", j))
+  
+  # row 3
+  row_header_1 <- c(row_header_1, "Totale sottopopolazione CAP")
+  j <- j + 1
+  tab_nice[, cell := as.character(N)]
+  setnames(tab_nice, "cell", paste0("cell_", j))
+  
+  # row 4-5
+  row_header_1 <- c(row_header_1, "Età alla data indice,", "  mediana (Q1 - Q3)")
+  j <- add_empty_row(j)
+  j <- descriptive_median_q1q3(j, "age")
+  
+  # row 6-10
+  row_header_1 <- c(row_header_1, 
+                    "Classi età alla data indice", 
+                    "  18-44, n (%)", 
+                    "  45-64, n (%)")
+  
+  j <- add_empty_row(j)
+  j <- descriptive_N_perc(j, "Age_18_44_")
+  j <- descriptive_N_perc(j, "Age_45_64_")
+  
+  # row 4
+  row_header_1 <- c(row_header_1, "BMI pregravidico basso (<18,5), n (%)")
+  j <- descriptive_N_perc(j, "bmi_low_")
+  
+  # row 5
+  row_header_1 <- c(row_header_1, "BMI pregravidico medio (18,5-24,9), n (%)")
+  j <- descriptive_N_perc(j, "bmi_medium_")
+  
+  # row 6
+  row_header_1 <- c(row_header_1, "BMI pregravidico alto (>24,9), n (%)")
+  j <- descriptive_N_perc(j, "bmi_high_")
+  
+  # row 7
+  row_header_1 <- c(row_header_1, "Diabete gestazionale, n (%)")
+  j <- descriptive_N_perc(j, "diab_gestaz_")
+  
+  # row 8
+  row_header_1 <- c(row_header_1, "Diabete pregravidico, n (%)")
+  j <- descriptive_N_perc(j, "diab_pregrav_")
+  
+  
+  #########################################
+  # KEEP CELLS
+  
+  cell_cols <- grep("^cell_", names(tab_nice), value = TRUE)
+  tokeep <- c("period", cell_cols)
+  tab_nice <- tab_nice[, ..tokeep]
+  
+  
+  #########################################
+  # RESHAPE CELLS
+  
+  # First reshape tab_nice from wide into long format
+  
+  tab_nice <- melt(
+    tab_nice,
+    id.vars = c("period"),
+    measure.vars = patterns(
+      cell = "^cell_[0-9]+$"
+    ),
+    variable.name = "rownum"
+  )
+  
+  tab_nice[, rownum := as.integer(rownum)]
+  
+  setorder(tab_nice, rownum, period)
+  
+  
+  # then reshape from long to wide keeping rownum as the UoO
+  
+  tab_nice <- dcast(
+    tab_nice,
+    rownum ~ period,
+    value.var = "value"
+  )
+  
+  
+  # Order rows correctly
+  setorder(tab_nice, rownum)
+  
+  
+  #########################################
+  # ADD ROW HEADER
+  # 
+  
+  tab_nice[, row_header := row_header_1]
+  tab_nice[, rownum := NULL]
+  
+  #########################################
+  # FINAL COLUMNS
+  
+  data_cols <- setdiff(names(tab_nice), "row_header")
+  pre_cols   <- grep("^pre", data_cols, value = TRUE)
+  nota_cols   <- grep("^nota", data_cols, value = TRUE)
+  other_cols <- setdiff(data_cols, c(pre_cols, nota_cols))
+  setcolorder(tab_nice, c("row_header", pre_cols, nota_cols, other_cols))
+  
+  #########################################
+  # NAMES
+  
+  newnames <- c(
+    pre = "Pre- Nota 100 AIFA (1ge2016-25gen2022)",
+    nota = "Nota 100 AIFA (26gen2022-31lug2025)",
+    modifica = "Modifica Nota 100 AIFA(1ago2025-31dic2025)"
+  )
+  
+  
+  tab_nice[1] <- lapply(tab_nice[1], function(x) {
+    
+    idx <- x %in% names(newnames)
+    x[idx] <- unname(newnames[x[idx]])
+    x
+    
+  })
+  
+  #########################################
+  # SAVE
+  
+  outputfile <- tab_nice
+  nameoutput <- "D6_Table_2_caratterizzazione_coorte_sottopopolazione_CAP"
+  assign(nameoutput, outputfile)
+  
+  # rds
+  saveRDS(outputfile, file = file.path(thisdiroutput, paste0(nameoutput,"_", k,"_overall.rds")))
+  # csv
+  fwrite(outputfile, file = file.path(thisdiroutput, paste0(nameoutput,"_", k,"_overall.csv")))
+  # xls
+  write_xlsx(outputfile, file.path(thisdiroutput, paste0(nameoutput,"_", k,"_overall.xlsx")))
+  # html
+  html_table <- kable(outputfile, format = "html", escape = FALSE) %>% kable_styling(full_width = F, bootstrap_options = c("striped", "hover"))
+  writeLines(html_table, file.path(thisdiroutput, paste0(nameoutput,"_", k,"_overall.html")))
+  # rtf
+  doc <- read_docx() %>% body_add_table(outputfile, style = "table_template", header = F) %>% body_end_section_continuous()
+  print(doc, target = file.path(thisdiroutput, paste0(nameoutput,"_", k,"_overall.docx")))
+
+}
+
+

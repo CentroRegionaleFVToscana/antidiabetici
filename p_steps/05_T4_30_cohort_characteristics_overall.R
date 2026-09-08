35f386605b3c72ddaa6853d1b46601ae150eefdb
sabrinagiometto <sabrina.giometto@gmail.com>
2026-09-08 15:43:49 +0200

    D5 for Table 1 including only overall frequencies added

---
+++ p_steps/05_T4_30_cohort_characteristics_overall.R
@@ -0,0 +1,114 @@
+# author: Sabrina Giometto
+
+# v 1.0 05 Giu 2026 Creation of D5 started
+
+# v 1.1 24 Giu 2026 Creation of D5 completed
+
+#########################################
+
+if (TEST){
+  testname <- "test_D5_Table_1_overall"
+  thisdirinput <- file.path(dirtest,testname)
+  thisdiroutput <- file.path(dirtest,testname,"g_output")
+  dir.create(thisdiroutput, showWarnings = F)
+}else{
+  thisdirinput <- dirtemp
+  thisdiroutput <- direxp
+}
+
+
+
+# load data
+
+# if (TEST & type_data_test=="simulation") {
+#   
+#   data <- readRDS(file = file.path(thisdirinput, "/D3_coorte_con_caratterizzazione.rds"))
+#   
+# } else if (TEST & type_data_test=="dummy") {
+#   
+#   data <- read_csv2(file.path(thisdirinput, "D3_coorte_con_caratterizzazione_dummy_data.csv"))
+#   
+# }
+
+for (i in drug_names) {
+
+  data <- readRDS(file = paste0(thisdirinput, "/D3_incidence_con_caratterizzazione_", i, ".rds"))
+  
+  data <- as.data.table(data)
+  
+  assign(paste0("D3_incidence_con_caratterizzazione_", i), data)
+
+}
+
+for (j in drug_names) {
+  
+  # Create D5 with sociodemographic characteristics
+  D5_nocov <- get(paste0("D3_incidence_con_caratterizzazione_", j))[, .(
+                N          = .N,
+                age_median = median(age),
+                age_q1 = quantile(age, probs = 0.25),
+                age_q3 = quantile(age, probs = 0.75),
+                Age_18_44_N = sum(ageband=="18-44"),
+                Age_18_44_p = round(sum(ageband=="18-44")/.N,3)*100,
+                Age_45_64_N = sum(ageband=="45-64"),
+                Age_45_64_p = round(sum(ageband=="45-64")/.N,3)*100,
+                Age_65_74_N = sum(ageband=="65-74"),
+                Age_65_74_p = round(sum(ageband=="65-74")/.N,3)*100,
+                Age_75over_N = sum(ageband=="75+"),
+                Age_75over_p = round(sum(ageband=="75+")/.N,3)*100,
+                genere_F_N = sum(genere=="F"),
+                genere_F_p = round(sum(genere=="F")/.N,3)*100),
+                .(period)]
+  
+  # create D5 with binary covariates
+
+  D5_cov <- NULL
+  
+  for (i in covariates_binary_tab1) {
+    
+    tmp <- get(paste0("D3_incidence_con_caratterizzazione_", j))[, .(
+                N = .N,
+                tmp_N = sum(get(i)==1),
+                tmp_p = round(sum(get(i)==1)/.N,3)*100),
+                .(period)]
+    
+    setnames(tmp,"tmp_N",paste0(i, "_N"))
+    setnames(tmp,"tmp_p",paste0(i, "_p"))
+    
+    if (is.null(D5_cov)) {
+      
+      D5_cov <- tmp
+      
+    } else {
+      
+      D5_cov <- merge(D5_cov, tmp, by = c("period", "N"))
+    }
+    
+  }
+  
+  # create the final D5 by merging the previous two
+  D5 <- merge(D5_nocov, D5_cov, by = c("period", "N"), all = F)
+  
+  assign(paste0("D5_",j), D5)
+
+}
+
+for (j in drug_names) {
+  
+  saveRDS(get(paste0("D5_", j)), file = paste0(thisdiroutput, "/D5_Table_1_caratterizzazione_coorte_", j, "_overall.rds"))
+  write.csv(get(paste0("D5_", j)), file = paste0(thisdiroutput, "/D5_Table_1_caratterizzazione_coorte_", j, "_overall.csv"))
+
+  # # save
+  # if (TEST & type_data_test=="simulation") {
+  #   
+  #   saveRDS(D5, file = paste0(thisdiroutput, "/D5_from_simulation_", j, ".rds"))
+  #   write.csv(D5, file = paste0(thisdiroutput, "/D5_from_simulation_", j, ".csv"))
+  #   
+  # } else if (TEST & type_data_test=="dummy") {
+  #   
+  #   saveRDS(D5, file = paste0(thisdiroutput, "/D5_from_dummy_data_", j,".rds"))
+  #   write.csv(D5, file = paste0(thisdiroutput, "/D5_from_dummy_data_", j,".csv"))
+  #   
+  # }
+  
+}

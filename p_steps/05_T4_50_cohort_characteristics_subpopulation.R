e9baae26b381d7037ee0ab817d5e95ac7e960b29
sabrinagiometto <sabrina.giometto@gmail.com>
2026-09-08 15:45:57 +0200

    number of the steps modified

---
+++ p_steps/05_T4_50_cohort_characteristics_subpopulation.R
@@ -0,0 +1,104 @@
+# author: Sabrina Giometto
+
+# v 1.0 09 Lug 2026 Creation of D5 started
+
+# v 1.1 14 Lug 2026 Creation of D5 completed
+
+#########################################
+
+if (TEST){
+  testname <- "test_D5_Table_2"
+  thisdirinput <- file.path(dirtest,testname)
+  thisdiroutput <- file.path(dirtest,testname,"g_output")
+  dir.create(thisdiroutput, showWarnings = F)
+}else{
+  thisdirinput <- dirinput
+  thisdiroutput <- direxp
+}
+
+# load data
+
+# if (TEST & type_data_test=="simulation") {
+#   
+#   data <- readRDS(file = file.path(thisdirinput, "/D3_coorte_con_caratterizzazione.rds"))
+#   
+# } else if (TEST & type_data_test=="dummy") {
+#   
+#   data <- read_csv2(file.path(thisdirinput, "D3_coorte_con_caratterizzazione_dummy_data.csv"))
+#   
+# }
+
+for (i in drug_names) {
+
+  data <- readRDS(file = file.path(thisdirinput, paste0("/D3_incidence_con_caratterizzazione_sottopop_CAP_", i, ".rds")))
+  
+  data <- as.data.table(data)
+  
+  assign(paste0("D3_incidence_con_caratterizzazione_sottopop_CAP_", i), data)
+
+}
+
+for (j in drug_names) {
+  
+  D5_nocov <- get(paste0("D3_incidence_con_caratterizzazione_sottopop_CAP_", j))[, .(
+    N          = .N,
+    age_median = median(age),
+    age_q1 = quantile(age, probs = 0.25),
+    age_q3 = quantile(age, probs = 0.75),
+    Age_18_44_N = sum(ageband=="18-44"),
+    Age_18_44_p = round(sum(ageband=="18-44")/.N,3)*100,
+    Age_45_64_N = sum(ageband=="45-64"),
+    Age_45_64_p = round(sum(ageband=="45-64")/.N,3)*100),
+    .(period, ASL)]
+
+  # create D5 with binary covariates
+  
+  D5_cov <- NULL
+  
+  for (i in covariates_binary_tab2) {
+    
+    tmp <- get(paste0("D3_incidence_con_caratterizzazione_sottopop_CAP_", j))[, .(
+      N = .N,
+      tmp_N = sum(get(i)==1),
+      tmp_p = round(sum(get(i)==1)/.N,3)*100),
+      .(period, ASL)]
+    
+    setnames(tmp,"tmp_N",paste0(i, "_N"))
+    setnames(tmp,"tmp_p",paste0(i, "_p"))
+      
+    if (is.null(D5_cov)) {
+      
+      D5_cov <- tmp
+      
+    } else {
+      
+      D5_cov <- merge(D5_cov, tmp, by = c("period", "ASL", "N"))
+    }
+      
+  }
+  
+  # create the final D5 by merging the previous two
+  D5 <- merge(D5_nocov, D5_cov, by = c("period", "ASL", "N"), all = F)
+  
+  assign(paste0("D5_sottopop_CAP_",j), D5)
+
+}
+
+# save
+for (j in drug_names) {
+  
+  saveRDS(get(paste0("D5_sottopop_CAP_", j)), file = paste0(thisdiroutput, "/D5_Table_2_caratterizzazione_coorte_sottopopolazione_CAP_", j, ".rds"))
+  write.csv(get(paste0("D5_sottopop_CAP_", j)), file = paste0(thisdiroutput, "/D5_Table_2_caratterizzazione_coorte_sottopopolazione_CAP_", j, ".csv"))
+
+  # if (TEST & type_data_test=="simulation") {
+  #   
+  #   saveRDS(D5, file = paste0(thisdiroutput, "/D5_from_simulation.rds"))
+  #   write.csv(D5, file = paste0(thisdiroutput, "/D5_from_simulation.csv"))
+  # 
+  # } else if (TEST & type_data_test=="dummy") {
+  #   
+  #   saveRDS(D5, file = paste0(thisdiroutput, "/D5_from_dummy_data.rds"))
+  #   write.csv(D5, file = paste0(thisdiroutput, "/D5_from_dummy_data.csv"))
+  #   
+  # }
+}

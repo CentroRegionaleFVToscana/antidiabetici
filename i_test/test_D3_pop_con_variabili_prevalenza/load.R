69a70dee26c57f8e720741519b2a6b0505275e35
RosaMGini <rosa.m.gini@gmail.com>
2026-09-24 18:41:19 +0200

    calcolate le variabili di prevalenza per ogni anno

---
+++ i_test/test_D3_pop_con_variabili_prevalenza/load.R
@@ -0,0 +1,64 @@
+rm(list=ls(all.names=TRUE))
+
+#set the directory where the script is saved as the working directory
+if (!require("rstudioapi")) install.packages("rstudioapi")
+thisdir <- setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+thisdir <- setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+# load packages
+if (!require("data.table")) install.packages("data.table")
+library(data.table)
+if (!require("lubridate")) install.packages("lubridate")
+library(lubridate)
+
+baselinedate <- 20151231
+
+# list of datasets
+listdatasetsRData <- c("SGLT2i")
+
+listdatasets <- c("D3_pop_SGLT2i",listdatasetsRData)
+
+
+# dates variables 
+
+listdates <- list()
+
+listdates[["D3_pop_SGLT2i"]] <- c("birth_date", "date_first", "end_study_op", "start_study_op")
+listdates[["SGLT2i"]] <- c("DATE")
+
+# date baseline
+
+baseline <- vector(mode="list")
+for (namedataset in listdatasets) {
+  for (datevar in listdates[[namedataset]]) {
+    baseline[[namedataset]][[datevar]] <- as.Date(lubridate::ymd(baselinedate))
+  }
+}
+
+
+# load datasets
+
+
+for (namedataset in listdatasets){
+  print(namedataset)
+  # data <- fread(paste0(thisdir, "/", namedataset, ".csv") )
+  # data[, (listdates[[namedataset]]) := lapply(.SD, lubridate::ymd), .SDcols = listdates[[namedataset]]]
+  data <- as.data.table(readxl::read_excel((paste0(thisdir, "/", namedataset, ".xlsx") )))
+  for (datevar in listdates[[namedataset]]) {
+    if (!is.na(baseline[[namedataset]][[datevar]])){
+      #  data[, (datevar) := as.Date(get(datevar), origin = "1970-01-01") + as.numeric(baseline[[namedataset]][[datevar]])]
+      data[, (datevar) := as.Date(get(datevar) + baseline[[namedataset]][[datevar]])]
+    }else{
+      data <- data[, (datevar) := lubridate::ymd(get(datevar))]
+      
+    }
+  }
+  
+  assign(namedataset,data)
+  if (namedataset %in% listdatasetsRData){
+    save(data, file = file.path(thisdir, paste0(namedataset,".RData")), list = namedataset)
+  }else{
+    saveRDS(data, file = file.path(thisdir, paste0(namedataset,".rds")))
+  }
+}
+
---
+++ p_steps/02_T2_40_create_D3_pop_con_variabili_prevalenza.R
@@ -0,0 +1,71 @@
+# author: Rosa Gini
+
+# v 1.0 24 Sep 2026
+#########################################
+
+if (TEST){
+  testname <- "test_D3_pop_con_variabili_prevalenza"
+  thisdirinput <- file.path(dirtest,testname)
+  thisdiroutput <- file.path(dirtest,testname,"g_output")
+  dir.create(thisdiroutput, showWarnings = F)
+  thisdrug_names <- c("SGLT2i")
+}else{
+  thisdirinput <- dirtemp
+  thisdiroutput <- dirtemp
+  thisdrug_names <- drug_names
+}
+
+i <- "SGLT2i"
+
+for (i in thisdrug_names) {
+  
+  print(i)
+  
+  # load data
+  
+  processing <- readRDS(file.path(thisdirinput, paste0("D3_pop_", i, ".rds")))
+  processing <- processing[, .(person_id, start_study_op,end_study_op, is_prevalent, date_first, birth_date)]
+  
+  processing[,date_18th_birthday := birth_date %m+% years(18)]  
+  
+  medicines <- as.data.table(get(load(file.path(thisdirinput, paste0(i,".RData")))[[1]]))
+  
+  medicines[, yearmed := year(DATE)]
+  
+  setnames(medicines, "ID", "person_id")
+  
+  medicines <- unique(medicines[,.(person_id, yearmed)])
+
+  
+  for (year in 2016:2025) {
+    medicines[, in_ := fifelse(yearmed == year, 1 ,0)]
+    temp <- copy(medicines)[in_ == 1,.(person_id, in_)]
+    processing <- merge(processing, temp, by = "person_id", all.x = T)
+    processing[end_study_op < ymd(paste0(year,"0101")), in_ := 0]
+    processing[date_18th_birthday >ymd(paste0(year,"1231")), in_ := 0]
+    processing[in_ == 1 & is_prevalent == 0 & year(date_first) == year , is_incident_:= 1]
+    processing[in_ == 1 & is_prevalent == 1  & year(date_first) == year , is_prevalent_ := 1]
+    processing[in_ == 1 &  year(date_first) < year , is_prevalent_ := 1]
+    processing[is.na(in_), in_ := 0]
+    processing[is.na(is_incident_), is_incident_ := 0]
+    processing[is.na(is_prevalent_), is_prevalent_ := 0]
+    
+    setnames(processing, c("in_", "is_prevalent_", "is_incident_"), paste0(c("in_", "is_prevalent_", "is_incident_"), year))
+    
+    medicines[, in_ := NULL]
+  }
+  
+  processing[, is_prevalent := NULL]
+  
+  # clean and save
+  
+  # tokeep <- c("person_id", "date_first", "period", "ASL", "age", "ageband", "genere", "met", "antidiabother", "IHD", "AMI", "bypass", "angioplastic", "STROKE", "TIA", "carot", "ateros", "organdamage", "age50plus", "dyslipidemia", "obesity", "hypertension", "smoking", "Cvriskfactors", "RENDIS_Alg1_1", "RENDIS_Alg1_2", "RENDIS_Alg1_3", "RENDIS_Alg1", "RENDIS_Alg2", "CV", "cerebro", "aop", "HF", "Cvrisk", "Cvtotal", "renal", "study_drugs","anyantidiab")
+  # 
+  # processing <- processing[, ..tokeep]
+
+  nameoutputfile <- paste0("D3_pop_con_variabili_prevalenza_", i, ".rds")
+
+  saveRDS(processing, file = file.path(thisdiroutput, nameoutputfile))
+  
+
+}
